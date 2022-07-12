8510dd8e30342b5158293b8520138bfec372482f
Sanidhya Kashyap <sanidhya.kashyap@epfl.ch>
2022-07-12 09:30:22 -0700

    update website

--- _static/custom.js
+++ _static/custom.js
@@ -1,3 +1,3 @@
 // create menu dynamically
-$("<a href='https://github.gatech.edu/cs3210-fall2017'>Submission Site</a>").insertAfter(".toctree-l1:contains('Schedule')");
+// $("<a href='https://github.gatech.edu/cs3210-fall2017'>Submission Site</a>").insertAfter(".toctree-l1:contains('Schedule')");
 $("<a href='https://piazza.com/gatech/fall2017/cs3210agr'>Piazza</a>").insertAfter(".toctree-l1:contains('Schedule')");
